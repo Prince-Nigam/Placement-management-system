02e15ee28a3f8699b0f1e1c1aebedaf7107fb188
Prince Nigam <princenigam972@gmail.com>
2026-07-14 12:19:59 +0530

    first component

--- force-app/main/default/lwc/firstComponent/firstComponent.js
+++ force-app/main/default/lwc/firstComponent/firstComponent.js
@@ -1,28 +1,20 @@
 import { LightningElement } from 'lwc';
 
 export default class FirstComponent extends LightningElement {
+    name = ' ';
+    rollNo = ' ';
 
-    isExecuted = false;
-
-    students = [
-        { id: 1, name: "Aman", branch: "CSE", cgpa: 8.5, placed: true, package: 1200000 },
-        { id: 2, name: "Rahul", branch: "IT", cgpa: 6.8, placed: false, package: 0 },
-        { id: 3, name: "Sneha", branch: "ECE", cgpa: 9.1, placed: true, package: 1800000 },
-        { id: 4, name: "Priya", branch: "CSE", cgpa: 7.2, placed: true, package: 800000 },
-        { id: 5, name: "Karan", branch: "Mechanical", cgpa: 5.9, placed: false, package: 0 },
-        { id: 6, name: "Neha", branch: "IT", cgpa: 8.9, placed: true, package: 1500000 }
-
-    ];
-
-    checkIfEligible(){
-        
-        this.students = this.students.map(student => {
-            return {
-                ...student,
-                isEligible: student.cgpa > 7
-            }
-        })
-        this.isExecuted = true;
+    storeName(event){
+        this.name = event.target.value;
+        console.log("Name : ",this.name);
     }
 
+    storeRollNo(event){
+        this.rollNo = event.target.value;
+        console.log('Roll No : ',this.rollNo);
+    }
+
+    createstudent(){
+        alert(`Student Created Successfully. Name : ${this.name} Roll Number : ${this.rollNo}`);
+    }
 }
